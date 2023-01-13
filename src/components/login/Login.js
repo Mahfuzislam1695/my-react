ce001fc360a1698e574f65aa5a6984a854bcb3ed
MAHFUZ ISLAM <77238922+Mahfuzislam1695@users.noreply.github.com>
2023-01-14 00:39:34 +0600

    first commit

---
+++ src/components/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LoginMainDiv } from './Login.style';
+
+const Login = () => {
+    return (
+        <LoginMainDiv>
+            this is login
+        </LoginMainDiv>
+    );
+};
+
+export default Login;
---
+++ src/components/login/Login.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const LoginMainDiv = styled.div`
+  display: flex;
+  position: relative;
+  min-height: 100vh; 
+`;
